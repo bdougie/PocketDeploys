c7563052574429bd7432659499e82314c575f91e
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-02-15 16:34:22 -0800

    adds images:

--- src/containers/SitesContainer.js
+++ src/containers/SitesContainer.js
@@ -1,9 +1,9 @@
 import React, {Component} from "react";
 import styled from 'styled-components/native';
-import {View, Text} from "react-native";
+import {StyleSheet, Image, View, Text} from "react-native";
 import {fetchSites}  from '../lib/netlify-api.js';
-// import {createContainer} from 'react-transmit-native';
 import {createContainer} from 'react-transmit';
+// import placeholder from "../images/placeholder.png";
 
 // Create a <Title> react component that renders an <h1> which is
 // centered, palevioletred and sized at 1.5em
@@ -19,18 +19,31 @@ const Wrapper = styled.View`
   padding: 55;
 `;
 
+const style = StyleSheet.create({
+  image: {
+    borderRadius: 3,
+    height: 60,
+    marginLeft: 10,
+    marginRight: 15,
+    width: 60,
+  },
+});
+
 class SitesContainer extends Component {
   render() {
     const {sites} = this.props
-    console.log(sites)
-    return (
+    const image = sites.screenshot_url || 'https://s3-us-west-1.amazonaws.com/publis-brian-images/netlify+images/placeholder.png'
+    console.log(image)
+
+    return sites ?
       <Wrapper>
         <StyledText>
           Netlify
         </StyledText>
+        <Image style={style.image} source={{uri: image}} />
         <Text>{sites.deploy_url}</Text>
       </Wrapper>
-    );
+      : <StyledText>...Loading</StyledText>;
   }
 }
 
--- src/lib/netlify-api.js
+++ src/lib/netlify-api.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 import Config from 'react-native-config'
 
-const url = `https://api.netlify.com/api/v1/sites?filter=all&access_token=${Config.ACCESS_TOKEN}`
+// const url = `https://api.netlify.com/api/v1/sites?filter=all&sort_by=updated_at&page=1&per_page=15&access_token=${Config.ACCESS_TOKEN}`
+const url = `https://api.netlify.com/api/v1/sites?access_token=${Config.ACCESS_TOKEN}`
 const headers = {'User-Agent': `MyApp (brian+test@netlify.com)`}
 
 export function login() {
-  return axios.post(url, {
+  return axios.get(url, {
     methos: 'POST',
     headers
   })
