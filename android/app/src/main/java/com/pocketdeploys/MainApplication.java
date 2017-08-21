18b46c2d81dcd0be3c730f1d9762bbf45171a5af
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-08-21 11:46:18 -0700

    adds code-push

--- android/app/src/main/java/com/pocketdeploys/MainApplication.java
+++ android/app/src/main/java/com/pocketdeploys/MainApplication.java
@@ -3,6 +3,10 @@ package com.pocketdeploys;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.azure.mobile.react.crashes.RNCrashesPackage;
+import com.microsoft.azure.mobile.react.analytics.RNAnalyticsPackage;
+import com.microsoft.azure.mobile.react.mobilecenter.RNMobileCenterPackage;
+import com.microsoft.codepush.react.CodePush;
 import com.horcrux.svg.SvgPackage;
 <<<<<<< ours
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
@@ -20,6 +24,12 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+    @Override
+    protected String getJSBundleFile() {
+      return CodePush.getJSBundleFile();
+    }
+
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -29,6 +39,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCrashesPackage(MainApplication.this, getResources().getString(R.string.mobileCenterCrashes_whenToSendCrashes)),
+            new RNAnalyticsPackage(MainApplication.this, getResources().getString(R.string.mobileCenterAnalytics_whenToEnableAnalytics)),
+            new RNMobileCenterPackage(MainApplication.this),
+            new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
             new SvgPackage(),
             new ReactNativeConfigPackage()
       );
